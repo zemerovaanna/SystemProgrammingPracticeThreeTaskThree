5e8586a24e39c2242ab11d30d774c8b3948d042e
cvv_code <cvv_code@gmail.com>
2024-02-03 11:18:25 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppWithoutDirectives.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	cout << "Вычисление значения у, при у = 1 - (1/4) * sin^2(2) * a + cos(2)a:" << endl;
+	double a;
+	cout << "Пожалуйста, введите а:";
+	cin >> a;
+	double y = 1 - 1 / 4 * pow(sin(2), 2) * a + cos(2) * a;
+	cout << "y = " << y;
+	return 0;
+}
