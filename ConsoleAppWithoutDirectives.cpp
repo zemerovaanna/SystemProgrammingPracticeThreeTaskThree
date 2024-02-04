22b5783497398fcf7854a413d6a5c987091f6306
Anna <123442002+zemerovaanna@users.noreply.github.com>
2024-02-04 10:52:06 +0500

    Update ConsoleAppWithoutDirectives.cpp

--- ConsoleAppWithoutDirectives.cpp
+++ ConsoleAppWithoutDirectives.cpp
@@ -1,14 +1,15 @@
-﻿#include <iostream>
+#include <iostream>
 #include <math.h>
 using namespace std;
 
-int main() {
-	setlocale(LC_ALL, "ru");
-	cout << "Вычисление значения у, при у = 1 - (1/4) * sin^2(2) * a + cos(2)a:" << endl;
-	double a;
-	cout << "Пожалуйста, введите а:";
-	cin >> a;
-	double y = 1 - 1 / 4 * pow(sin(2), 2) * a + cos(2) * a;
-	cout << "y = " << y;
-	return 0;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Задание 3: Вычислить y, при y = 1 - (1/4) * sin^2(a) + cos(2) a." << endl;
+    int a;
+    cout << "Пожалуйста, введите a: ";
+    cin >> a;
+    double y = 1 - (1 / 4) * pow(sin(a), 2) + cos(2) * a;
+    cout << "y = " << y << endl;
+    return 0;
 }
